1d365d5f67050fe7be4707fabf9a57b365538137
Asfandiyark007 <asfandiyar.bsee261@iiu.edu.pk>
2025-06-25 20:49:32 +0500

    Cli-Cache with persistent Cache and Expiry

--- CLI-Cache.py
+++ CLI-Cache.py
@@ -1,21 +1,15 @@
-import cmd
+import click
 
 
-# This is a simple command-line interface (CLI) cache example using the cmd
-#  module in Python.
-class SimpleShell(cmd.Cmd):
-    prompt = '> '
-    intro = "Welcome to the Simple Shell! Type help or ? to list commands."
-
-    def do_greet(self, name):
-        """Greet the user by name."""
-        print(f"Hello, {name}!")
-
-    def do_exit(self, _):
-        """Exit the shell."""
-        print("Goodbye!")
-        return True
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
 
 
 if __name__ == '__main__':
-    SimpleShell().cmdloop()
+    hello()
